3b6d22cdb0d151aa6dcff6e031370038fc4875ce
Xing_Ye <YOUR_PERSONAL_EMAIL@example.com>
2019-09-18 09:27:35 -0700

    gained access input query

--- app.py
+++ app.py
@@ -8,32 +8,34 @@ app = Flask(__name__)
 def index():
     """Return homepage."""
     # TODO: Extract the query term from url using request.args.get()
-    return render_template('index.html')
+
     # TODO: Make 'params' dictionary containing:
     # a) the query term, 'q'
     # b) your API key, 'key'
     # c) how many GIFs to return, 'limit'
+    query = request.args.get("name") #how to access input query
     params = {
-        "q": "query",
-        "apikey": "7KOU0A3XQ2BG",
+        "q": query,
+        "key": "7KOU0A3XQ2BG",
         "limit" : 10
     }
-    response = request.get(
-        'http://api.tenor.com/v1/search',
-        params=params
-    )
+
+    #search_term = "smile"
     # TODO: Make an API call to Tenor using the 'requests' library. For 
     # reference on how to use Tenor, see: 
     # https://tenor.com/gifapi/documentation
-    r = requests.get("https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s" % (params['q'], params['apikey'], limit))
+    r = requests.get("https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s" % (params['q'],params['key'],params['limit']) )
     
     # TODO: Use the '.json()' function to get the JSON of the returned response
     # object
-    if r.status_code == 200:
-        gifs = json.loads(r.content)
+    gifs = json.loads(r.content)
+    print(gifs)
+
+    gif_results = []
+    for i in range params['limit']:
         
-    else:
-        gifs = None
+
+
 
     # TODO: Using dictionary notation, get the 'results' field of the JSON,
     # which contains the GIFs as a list
@@ -42,7 +44,7 @@ def index():
     # TODO: Render the 'index.html' template, passing the list of gifs as a
     # named parameter called 'gifs'
 
-    return render_template("index.html")
+    return render_template("index.html", gifs=gifs)
 
 if __name__ == '__main__':
     app.run(debug=True)
